b3eaa26986e2214c18f9906dc328c4e15c839f30
VU TUAN TRUNG <trung.lespace@gmail.com>
2024-01-22 22:47:42 +0700

    fix + configuring owl carousel not working

--- src/pages/dashboard/Header/components/CategoryItem.tsx
+++ src/pages/dashboard/Header/components/CategoryItem.tsx
@@ -1,10 +1,4 @@
-export const CategoryItem = ({
-    name,
-    href,
-}: {
-    name: string;
-    href: string;
-}) => {
+export const CategoryItem = ({ name, href }: { name: string; href: string }) => {
     return (
         <a href={href} className="item channel_item">
             {name}
